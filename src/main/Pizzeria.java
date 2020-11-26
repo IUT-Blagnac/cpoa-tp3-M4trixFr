283bb6cb63b81662a60542384fd1f45948e041f7
mathieu.beth <mathieu3232@gmail.com>
2020-11-26 10:36:47 +0100

    fix #0 Initial task done

---
+++ src/main/Pizzeria.java
@@ -0,0 +1,30 @@
+/**
+ * @author bruel (from O'Reilly Head-First series)
+ * @depend - * - Pizza
+ */
+public class Pizzeria {
+
+        /**
+         * @param type
+         * @return a Pizza object according to the type
+         */
+		public Pizza commanderPizza(String type) {
+
+		Pizza pizza;
+		
+		if (type.equals("fromage")) {
+			pizza = new PizzaFromage();
+		} else if (type.equals("grecque")) {
+			pizza = new PizzaGrecque();
+		} else {
+			pizza = new PizzaPoivrons();
+		}
+
+		pizza.preparer();
+		pizza.cuire();
+		pizza.couper();
+		pizza.emballer();
+
+		return pizza;
+		}
+}
